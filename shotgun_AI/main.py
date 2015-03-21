46777b702da9b045a994cb1fa3341deb2167949f
David Greydanus <davidgreydanus@gmail.com>
2015-03-20 23:11:00 -0400

    Update main.py

--- shotgun_AI/main.py
+++ shotgun_AI/main.py
@@ -1,5 +1,4 @@
 """
-
 The MIT License (MIT)
 Copyright (c) 2015 David Greydanus
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,7 +16,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
-
 '''           
 Created on Dec 15, 2013
 Overhauled  on Jan 22, 2015
